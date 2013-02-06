05e656b0dea562c8a6a7dbf4359e711cf5e85dd5
Arnaud Dumont <arnaud.dumont@ausregistry.com.au>
2013-02-06 16:43:40 +1100

    #585: Dan & Arnaud: Added DomainDeleteApplicationCommandExtension. Release 2.0.6.12

---
+++ java/src/main/java/com/ausregistry/jtoolkit2/launch/DomainDeleteApplicationCommandExtension.java
@@ -0,0 +1,29 @@
+package com.ausregistry.jtoolkit2.launch;
+
+import com.ausregistry.jtoolkit2.se.Command;
+import com.ausregistry.jtoolkit2.se.CommandExtension;
+import com.ausregistry.jtoolkit2.se.ExtendedObjectType;
+import com.ausregistry.jtoolkit2.xml.XMLWriter;
+import org.w3c.dom.Element;
+
+public class DomainDeleteApplicationCommandExtension implements CommandExtension {
+
+    private static final long serialVersionUID = 5202343696850193788L;
+    private String applicationId;
+
+    @Override
+    public void addToCommand(Command command) throws Exception {
+        final XMLWriter xmlWriter = command.getXmlWriter();
+        final Element extensionElement = command.getExtensionElement();
+        final Element createElement = xmlWriter.appendChild(extensionElement, "delete",
+                ExtendedObjectType.LAUNCH.getURI());
+
+        xmlWriter.appendChild(createElement, "id", ExtendedObjectType.LAUNCH.getURI()).setTextContent
+                (applicationId);
+
+    }
+
+    public void setApplicationId(String applicationId) {
+        this.applicationId = applicationId;
+    }
+}
---
+++ java/src/test/java/com/ausregistry/jtoolkit2/gtld/launch/DomainDeleteApplicationCommandExtensionTest.java
@@ -0,0 +1,41 @@
+package com.ausregistry.jtoolkit2.gtld.launch;
+
+import static org.junit.Assert.assertEquals;
+
+import com.ausregistry.jtoolkit2.Timer;
+import com.ausregistry.jtoolkit2.launch.DomainDeleteApplicationCommandExtension;
+import com.ausregistry.jtoolkit2.se.CLTRID;
+import com.ausregistry.jtoolkit2.se.Command;
+import com.ausregistry.jtoolkit2.se.DomainDeleteCommand;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+public class DomainDeleteApplicationCommandExtensionTest {
+
+    @Before
+    public void setUp() throws Exception {
+        Timer.setTime("20070101.010101");
+        CLTRID.setClID("JTKUTEST");
+    }
+
+    @Test
+    public void shouldCreateValidXmlWhenSupplyLaunchExtensionForDelete() throws SAXException {
+
+        final Command cmd = new DomainDeleteCommand("jtkutest.com.au");
+        final DomainDeleteApplicationCommandExtension ext = new DomainDeleteApplicationCommandExtension();
+        ext.setApplicationId("sunrise-application-id");
+
+        cmd.appendExtension(ext);
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?><epp xmlns=\"urn:ietf:params:xml:ns:epp-1.0\""
+                + " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\""
+                + " xsi:schemaLocation=\"urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd\">"
+                + "<command><delete><delete xmlns=\"urn:ietf:params:xml:ns:domain-1.0\" "
+                + "xsi:schemaLocation=\"urn:ietf:params:xml:ns:domain-1.0 domain-1.0.xsd\">"
+                + "<name>jtkutest.com.au</name></delete></delete>" + "<extension>"
+                + "<delete xmlns=\"urn:rbp:params:xml:ns:application-1.0\"><id>sunrise-application-id</id></delete>"
+                + "</extension><clTRID>JTKUTEST.20070101.010101.0</clTRID></command></epp>", cmd.toXML());
+
+    }
+
+}
