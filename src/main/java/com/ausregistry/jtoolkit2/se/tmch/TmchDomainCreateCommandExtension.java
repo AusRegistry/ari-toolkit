5be30d8d108f321d2374cc5e1f64a1f77c18dd8a
Dan Lee <dan.lee@ausregistry.com.au>
2013-04-18 16:06:31 +1000

    #1157, Simon & Dan : Added Claims Notice Id in TmchDomainCreateCommandExtension.

--- src/main/java/com/ausregistry/jtoolkit2/se/tmch/TmchDomainCreateCommandExtension.java
+++ src/main/java/com/ausregistry/jtoolkit2/se/tmch/TmchDomainCreateCommandExtension.java
@@ -1,5 +1,8 @@
 package com.ausregistry.jtoolkit2.se.tmch;
 
+import java.util.GregorianCalendar;
+
+import com.ausregistry.jtoolkit2.EPPDateFormatter;
 import com.ausregistry.jtoolkit2.se.Command;
 import com.ausregistry.jtoolkit2.se.CommandExtension;
 import com.ausregistry.jtoolkit2.se.DomainCreateCommand;
@@ -11,9 +14,9 @@ import org.w3c.dom.Element;
  * <p>Extension for the EPP Domain Create command, representing the Create aspect of the
  * Domain Name Trademark Clearing House extension.</p>
  *
- * <p>Use this to identify the domain's encoded signed mark data as part of the result of this command, which is being
- * submitted in as part of an EPP Domain Create command compliant with RFC5730 and RFC5731. The response expected
- * from a server should be handled by a Domain Create Response.</p>
+ * <p>Use this to identify the domain's encoded signed mark data or notice id as part of the result of this command,
+ * which is being submitted in as part of an EPP Domain Create command compliant with RFC5730 and RFC5731.
+ * The response expected from a server should be handled by a Domain Create Response.</p>
  *
  * @see DomainCreateCommand
  * @see <a href="http://ausregistry.github.io/doc/tmch-1.0/tmch-1.0.html">Domain Name Trademark Clearing House
@@ -24,6 +27,9 @@ public class TmchDomainCreateCommandExtension implements CommandExtension {
     private static final long serialVersionUID = 2000050396747484091L;
 
     private String encodedSignedMarkData;
+    private String noticeId;
+    private GregorianCalendar notAfterDateTime;
+    private GregorianCalendar acceptedDateTime;
 
     @Override
     public void addToCommand(Command command) {
@@ -31,11 +37,42 @@ public class TmchDomainCreateCommandExtension implements CommandExtension {
         final Element extensionElement = command.getExtensionElement();
         final Element tmchCreateElement = xmlWriter.appendChild(extensionElement, "create",
                 ExtendedObjectType.TMCH.getURI());
-        xmlWriter.appendChild(tmchCreateElement, "smd", ExtendedObjectType.TMCH.getURI())
-                .setTextContent(encodedSignedMarkData);
+
+        if (encodedSignedMarkData != null) {
+            xmlWriter.appendChild(tmchCreateElement, "smd", ExtendedObjectType.TMCH.getURI())
+                    .setTextContent(encodedSignedMarkData);
+
+        }
+
+        if (noticeId != null) {
+            xmlWriter.appendChild(tmchCreateElement, "noticeID", ExtendedObjectType.TMCH.getURI())
+                    .setTextContent(noticeId);
+        }
+
+        if (notAfterDateTime != null) {
+            xmlWriter.appendChild(tmchCreateElement, "notAfter", ExtendedObjectType.TMCH.getURI())
+                    .setTextContent(EPPDateFormatter.toXSDateTime(notAfterDateTime));
+        }
+
+        if (acceptedDateTime != null) {
+            xmlWriter.appendChild(tmchCreateElement, "accepted", ExtendedObjectType.TMCH.getURI())
+                    .setTextContent(EPPDateFormatter.toXSDateTime(acceptedDateTime));
+        }
     }
 
     public void setEncodedSignedMarkData(String encodedSignedMarkData) {
         this.encodedSignedMarkData = encodedSignedMarkData;
+    }
+
+    public void setNoticeId(String noticeId) {
+        this.noticeId = noticeId;
+    }
+
+    public void setNotAfterDateTime(GregorianCalendar notAfterDateTime) {
+        this.notAfterDateTime = notAfterDateTime;
+    }
+
+    public void setAcceptedDateTime(GregorianCalendar acceptedDateTime) {
+        this.acceptedDateTime = acceptedDateTime;
     }
 }
--- src/test/java/com/ausregistry/jtoolkit2/se/tmch/TmchDomainCreateCommandExtensionTest.java
+++ src/test/java/com/ausregistry/jtoolkit2/se/tmch/TmchDomainCreateCommandExtensionTest.java
@@ -2,43 +2,111 @@ package com.ausregistry.jtoolkit2.se.tmch;
 
 import static org.junit.Assert.assertEquals;
 
+import com.ausregistry.jtoolkit2.EPPDateFormatter;
 import com.ausregistry.jtoolkit2.Timer;
 import com.ausregistry.jtoolkit2.se.CLTRID;
-import com.ausregistry.jtoolkit2.se.Command;
 import com.ausregistry.jtoolkit2.se.DomainCreateCommand;
-import com.ausregistry.jtoolkit2.xml.XMLParser;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.xml.sax.SAXException;
 
 public class TmchDomainCreateCommandExtensionTest {
 
-    private static final XMLParser PARSER = new XMLParser();
+    private final static String DOMAIN_CREATE_WITH_TMCH_SMD_XML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+            + "<epp xmlns=\"urn:ietf:params:xml:ns:epp-1.0\" "
+            + "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" "
+            + "xsi:schemaLocation=\"urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd\">"
+            + "<command>"
+            + "<create>"
+            + "<create xmlns=\"urn:ietf:params:xml:ns:domain-1.0\" "
+            + "xsi:schemaLocation=\"urn:ietf:params:xml:ns:domain-1.0 domain-1.0.xsd\">"
+            + "<name>tmch.example</name>"
+            + "<authInfo><pw>jtkUT3st</pw></authInfo>"
+            + "</create>"
+            + "</create>"
+            + "<extension>"
+            + "<create xmlns=\"urn:ar:params:xml:ns:tmch-1.0\">"
+            + "<smd>ZW5jb2RlZFNpZ25lZE1hcmtEYXRh</smd>"
+            + "</create>"
+            + "</extension>"
+            + "<clTRID>JTKUTEST.20070101.010101.0</clTRID>"
+            + "</command>"
+            + "</epp>";
+
+
+    private final static String DOMAIN_CREATE_WITH_TMCH_NOTICE_ID_XML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+            + "<epp xmlns=\"urn:ietf:params:xml:ns:epp-1.0\" "
+            + "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" "
+            + "xsi:schemaLocation=\"urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd\">"
+            + "<command>"
+            + "<create>"
+            + "<create xmlns=\"urn:ietf:params:xml:ns:domain-1.0\" "
+            + "xsi:schemaLocation=\"urn:ietf:params:xml:ns:domain-1.0 domain-1.0.xsd\">"
+            + "<name>tmch.example</name>"
+            + "<authInfo><pw>jtkUT3st</pw></authInfo>"
+            + "</create>"
+            + "</create>"
+            + "<extension>"
+            + "<create xmlns=\"urn:ar:params:xml:ns:tmch-1.0\">"
+            + "<noticeID>49FD46E6C4B45C55D4AC</noticeID>"
+            + "<notAfter>2007-01-01T01:01:01.000Z</notAfter>"
+            + "<accepted>2007-02-02T02:02:02.000Z</accepted>"
+            + "</create>"
+            + "</extension>"
+            + "<clTRID>JTKUTEST.20070101.010101.0</clTRID>"
+            + "</command>"
+            + "</epp>";
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private DomainCreateCommand domainCreateCommand;
+    private TmchDomainCreateCommandExtension tmchDomainCreateCommandExtension;
 
     @Before
     public void setUp() throws Exception {
         Timer.setTime("20070101.010101");
         CLTRID.setClID("JTKUTEST");
+
+        domainCreateCommand = new DomainCreateCommand("tmch.example", "jtkUT3st");
+        tmchDomainCreateCommandExtension = new TmchDomainCreateCommandExtension();
     }
 
     @Test
-    public void shouldCreateValidXmlWhenSupplyTmchExtension() throws SAXException {
+    public void shouldCreateValidXmlWhenSupplyingTmchExtensionWithSMD() throws SAXException {
+        tmchDomainCreateCommandExtension.setEncodedSignedMarkData("ZW5jb2RlZFNpZ25lZE1hcmtEYXRh");
+        domainCreateCommand.appendExtension(tmchDomainCreateCommandExtension);
 
-        final Command cmd = new DomainCreateCommand("jtkutest.com.au", "jtkUT3st");
-        final TmchDomainCreateCommandExtension ext = new TmchDomainCreateCommandExtension();
-        String encodedSignedMarkData = "ZW5jb2RlZFNpZ25lZE1hcmtEYXRh"; // 'encodedSignedMarkData'
-        ext.setEncodedSignedMarkData(encodedSignedMarkData);
-
-        cmd.appendExtension(ext);
-        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?><epp xmlns=\"urn:ietf:params:xml:ns:epp-1.0\""
-                + " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\""
-                + " xsi:schemaLocation=\"urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd\">"
-                + "<command><create><create xmlns=\"urn:ietf:params:xml:ns:domain-1.0\" "
-                + "xsi:schemaLocation=\"urn:ietf:params:xml:ns:domain-1.0 domain-1.0.xsd\">"
-                + "<name>jtkutest.com.au</name><authInfo><pw>jtkUT3st</pw></authInfo></create></create>"
-                + "<extension><create xmlns=\"urn:ar:params:xml:ns:tmch-1.0\"><smd>"
-                + encodedSignedMarkData + "</smd></create>"
-                + "</extension><clTRID>JTKUTEST.20070101.010101.0</clTRID></command></epp>", cmd.toXML());
-
+        assertEquals(DOMAIN_CREATE_WITH_TMCH_SMD_XML, domainCreateCommand.toXML());
     }
+
+    @Test
+    public void shouldCreateValidXmlWhenSupplyingTmchExtensionWithNoticeId() throws SAXException {
+        tmchDomainCreateCommandExtension.setNoticeId("49FD46E6C4B45C55D4AC");
+        tmchDomainCreateCommandExtension.setNotAfterDateTime(
+                EPPDateFormatter.fromXSDateTime("2007-01-01T01:01:01.0Z"));
+        tmchDomainCreateCommandExtension.setAcceptedDateTime(
+                EPPDateFormatter.fromXSDateTime("2007-02-02T02:02:02.0Z"));
+        domainCreateCommand.appendExtension(tmchDomainCreateCommandExtension);
+
+        assertEquals(DOMAIN_CREATE_WITH_TMCH_NOTICE_ID_XML, domainCreateCommand.toXML());
+    }
+
+    @Test
+    public void shouldThrowParsingExceptionWhenProvidingBothSMDAndNoticeIdAsTmchExtension() throws SAXException {
+        tmchDomainCreateCommandExtension.setEncodedSignedMarkData("ZW5jb2RlZFNpZ25lZE1hcmtEYXRh");
+        tmchDomainCreateCommandExtension.setNoticeId("49FD46E6C4B45C55D4AC");
+        tmchDomainCreateCommandExtension.setNotAfterDateTime(
+                EPPDateFormatter.fromXSDateTime("2007-01-01T01:01:01.0Z"));
+        tmchDomainCreateCommandExtension.setAcceptedDateTime(
+                EPPDateFormatter.fromXSDateTime("2007-02-02T02:02:02.0Z"));
+        domainCreateCommand.appendExtension(tmchDomainCreateCommandExtension);
+
+        thrown.expect(SAXException.class);
+
+        domainCreateCommand.toXML();
+    }
+
 }
