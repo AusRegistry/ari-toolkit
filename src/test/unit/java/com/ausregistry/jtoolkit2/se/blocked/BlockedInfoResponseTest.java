942462085ef747e5195872c9306d9223218ee483
rcane <richardc@ausregistry.com.au>
2015-03-03 10:59:45 +1100

    IP-190: Fix info

--- src/test/unit/java/com/ausregistry/jtoolkit2/se/blocked/BlockedInfoResponseTest.java
+++ src/test/unit/java/com/ausregistry/jtoolkit2/se/blocked/BlockedInfoResponseTest.java
@@ -16,7 +16,7 @@ public class BlockedInfoResponseTest {
 
     @Test
     public void testResponse() throws Exception {
-        String id = UUID.randomUUID().toString();
+        String id = "BD-001";
         String blockedDomainInfoXml =
                 "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" +
                 "<epp xmlns=\"urn:ietf:params:xml:ns:epp-1.0\" " +
