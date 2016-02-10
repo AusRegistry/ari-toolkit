105fee4ce318b789e8eda5c7531c546c4effe0ec
rcane <richardc@ausregistry.com.au>
2016-02-10 12:26:15 +1100

    RPE-150: Add launch check extension

---
+++ src/test/unit/java/com/ausregistry/jtoolkit2/se/launch/DomainCheckLaunchCommandExtensionTest.java
@@ -0,0 +1,55 @@
+package com.ausregistry.jtoolkit2.se.launch;
+
+import static org.junit.Assert.assertEquals;
+
+import com.ausregistry.jtoolkit2.Timer;
+import com.ausregistry.jtoolkit2.se.CLTRID;
+import com.ausregistry.jtoolkit2.se.Command;
+import com.ausregistry.jtoolkit2.se.DomainCheckCommand;
+import com.ausregistry.jtoolkit2.xml.XMLParser;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+
+public class DomainCheckLaunchCommandExtensionTest {
+
+
+    private static final XMLParser PARSER = new XMLParser();
+
+    @Before
+    public void setUp() throws Exception {
+        Timer.setTime("20070101.010101");
+        CLTRID.setClID("JTKUTEST");
+    }
+
+    @Test
+    public void shouldCreateValidXmlWhenSupplyLaunchExtension() throws SAXException {
+
+        final Command cmd = new DomainCheckCommand("domain-name.unit-test");
+        final DomainCheckLaunchCommandExtension ext = new DomainCheckLaunchCommandExtension();
+        ext.setPhaseName("sunrise-fcfs");
+        ext.setPhaseType(PhaseType.SUNRISE);
+        cmd.appendExtension(ext);
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+                + "<epp xmlns=\"urn:ietf:params:xml:ns:epp-1.0\""
+                + " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\""
+                + " xsi:schemaLocation=\"urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd\">"
+                + "<command>"
+                + "<check>"
+                + "<check xmlns=\"urn:ietf:params:xml:ns:domain-1.0\""
+                + " xsi:schemaLocation=\"urn:ietf:params:xml:ns:domain-1.0 domain-1.0.xsd\">"
+                + "<name>domain-name.unit-test</name>"
+                + "</check>"
+                + "</check>"
+                + "<extension><check xmlns=\"urn:ietf:params:xml:ns:launch-1.0\" type=\"claims\">"
+                + "<phase name=\"sunrise-fcfs\">sunrise</phase>"
+                + "</check>"
+                + "</extension>"
+                + "<clTRID>JTKUTEST.20070101.010101.0</clTRID>"
+                + "</command>"
+                + "</epp>", cmd.toXML());
+
+    }
+}
+
