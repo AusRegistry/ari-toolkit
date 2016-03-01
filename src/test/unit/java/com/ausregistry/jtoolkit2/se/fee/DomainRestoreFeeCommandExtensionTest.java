0177580a3b87408f9dfa1bba8f064f1a06f0d0db
afeltner <andrea.feltner@neustar.biz>
2016-03-01 13:34:07 -0500

    RPE-202: Fixed some checkstyle errors

--- src/test/unit/java/com/ausregistry/jtoolkit2/se/fee/DomainRestoreFeeCommandExtensionTest.java
+++ src/test/unit/java/com/ausregistry/jtoolkit2/se/fee/DomainRestoreFeeCommandExtensionTest.java
@@ -15,7 +15,8 @@ import org.xml.sax.SAXException;
 
 import java.math.BigDecimal;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class DomainRestoreFeeCommandExtensionTest {
 
@@ -106,24 +107,24 @@ public class DomainRestoreFeeCommandExtensionTest {
                                                         boolean withRenew) {
         final StringBuilder result = new StringBuilder();
         result.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-        result.append( "<epp xmlns=\"urn:ietf:params:xml:ns:epp-1.0\"");
-        result.append( " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"");
-        result.append( " xsi:schemaLocation=\"urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd\">");
-        result.append( "<command>");
-        result.append( "<renew>");
-        result.append( "<renew xmlns=\"urn:ietf:params:xml:ns:domain-1.0\"");
-        result.append( " xsi:schemaLocation=\"urn:ietf:params:xml:ns:domain-1.0 domain-1.0.xsd\">");
-        result.append( "<name>" + domainName + "</name>");
-        result.append( "<curExpDate>2016-02-14</curExpDate>");
-        result.append( "<period unit=\"y\">1</period>");
-        result.append( "</renew>");
-        result.append( "</renew>");
-        result.append( "<extension>");
-        result.append( "<extension xmlns=\"urn:ietf:params:xml:ns:neulevel-1.0\">");
-        result.append( "<unspec>");
-        result.append( "RestoreReasonCode=1 RestoreComment=_My_Comment_ TrueData=Y ValidUse=Y");
-        result.append( "</unspec>");
-        result.append( "</extension>");
+        result.append("<epp xmlns=\"urn:ietf:params:xml:ns:epp-1.0\"");
+        result.append(" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"");
+        result.append(" xsi:schemaLocation=\"urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd\">");
+        result.append("<command>");
+        result.append("<renew>");
+        result.append("<renew xmlns=\"urn:ietf:params:xml:ns:domain-1.0\"");
+        result.append(" xsi:schemaLocation=\"urn:ietf:params:xml:ns:domain-1.0 domain-1.0.xsd\">");
+        result.append("<name>" + domainName + "</name>");
+        result.append("<curExpDate>2016-02-14</curExpDate>");
+        result.append("<period unit=\"y\">1</period>");
+        result.append("</renew>");
+        result.append("</renew>");
+        result.append("<extension>");
+        result.append("<extension xmlns=\"urn:ietf:params:xml:ns:neulevel-1.0\">");
+        result.append("<unspec>");
+        result.append("RestoreReasonCode=1 RestoreComment=_My_Comment_ TrueData=Y ValidUse=Y");
+        result.append("</unspec>");
+        result.append("</extension>");
         if (feeExtension) {
             result.append("<renew xmlns=\"urn:ietf:params:xml:ns:fee-0.6\">");
             result.append("<currency>USD</currency>");
@@ -135,10 +136,10 @@ public class DomainRestoreFeeCommandExtensionTest {
             }
             result.append("</renew>");
         }
-        result.append( "</extension>");
-        result.append( "<clTRID>JTKUTEST.20070101.010101.0</clTRID>");
-        result.append( "</command>");
-        result.append( "</epp>");
+        result.append("</extension>");
+        result.append("<clTRID>JTKUTEST.20070101.010101.0</clTRID>");
+        result.append("</command>");
+        result.append("</epp>");
         return result.toString();
     }
 
