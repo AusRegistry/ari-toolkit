f3483e9051aba89f3871ddcc015d7426d26661ee
Thisara Rupasinghe <thisara.rupasinghe@neustar.biz>
2016-04-18 16:29:43 +1000

    RPE-285 Add new fee response extension for application create

--- src/main/java/com/ausregistry/jtoolkit2/se/fee/DomainApplicationFeeResponseExtension.java
+++ src/main/java/com/ausregistry/jtoolkit2/se/fee/DomainApplicationFeeResponseExtension.java
@@ -3,16 +3,18 @@ package com.ausregistry.jtoolkit2.se.fee;
 import javax.xml.xpath.XPathExpressionException;
 import java.math.BigDecimal;
 
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import com.ausregistry.jtoolkit2.se.ExtendedObjectType;
 import com.ausregistry.jtoolkit2.se.ResponseExtension;
 import com.ausregistry.jtoolkit2.xml.XMLDocument;
 
 /**
  * <p>Extension for the EPP Domain Create response, representing the Fee
- * extension.</p>
+ * extension during a launch phase to create an application..</p>
  *
  * <p>Use this to acknowledge the price associated with this domain name as part of an EPP Domain Create
- * command compliant with RFC5730 and RFC5731. The "currency" and "registrationFee"
+ * command compliant with RFC5730 and RFC5731. The "currency", "applicationFee", "allocationFee" and "registrationFee"
  * values supplied, should match the fees that are set for the domain name for the requested period.
  * The response expected from a server should be handled by a Domain Create Response.</p>
  *
@@ -21,7 +23,7 @@ import com.ausregistry.jtoolkit2.xml.XMLDocument;
  * @see <a href="https://tools.ietf.org/html/draft-brown-epp-fees-03">Domain Name Fee Extension
  * Mapping for the Extensible Provisioning Protocol (EPP)</a>
  */
-public final class DomainCreateFeeResponseExtension extends ResponseExtension {
+public final class DomainApplicationFeeResponseExtension extends ResponseExtension {
 
     private static final long serialVersionUID = -6007874008986690757L;
 
@@ -30,29 +32,44 @@ public final class DomainCreateFeeResponseExtension extends ResponseExtension {
     private static final String FEE_XPATH_PREFIX = ResponseExtension.EXTENSION_EXPR + "/" + FEE_PREFIX
             + ":RESPONSE_TYPE/" + FEE_PREFIX;
     private static final String CURRENCY_EXPR = FEE_XPATH_PREFIX + ":currency/text()";
-    private static final String FEE_EXPR = FEE_XPATH_PREFIX + ":fee/text()";
+    private static final String FEE_EXPR = FEE_XPATH_PREFIX + ":fee";
 
     private String responseType;
 
     private boolean initialised = false;
 
     private String currency;
+    private BigDecimal applicationFee;
+    private BigDecimal allocationFee;
     private BigDecimal registrationFee;
 
-    public DomainCreateFeeResponseExtension(String responseType) {
+    public DomainApplicationFeeResponseExtension(String responseType) {
         this.responseType = responseType;
     }
 
     @Override
     public void fromXML(XMLDocument xmlDoc) throws XPathExpressionException {
         currency = xmlDoc.getNodeValue(replaceResponseType(CURRENCY_EXPR, responseType));
-        String feeNodeValue = xmlDoc.getNodeValue(replaceResponseType(FEE_EXPR, responseType));
-        if (feeNodeValue != null) {
-            registrationFee = new BigDecimal(feeNodeValue);
+        NodeList feeNodeList = xmlDoc.getElements(replaceResponseType(FEE_EXPR, responseType));
+        if (feeNodeList != null) {
+            int nodeLength = feeNodeList.getLength();
+            for (int i = 0; i < nodeLength; i++) {
+                Node node = feeNodeList.item(i);
+                String description = node.getAttributes().getNamedItem("description").getNodeValue();
+                if (description.equals("Application Fee")) {
+                    applicationFee = new BigDecimal(node.getTextContent());
+
+                } else if (description.equals("Allocation Fee")) {
+                    allocationFee = new BigDecimal(node.getTextContent());
+
+                } else if (description.equals("Registration Fee")) {
+                    registrationFee = new BigDecimal(node.getTextContent());
+
+                }
+            }
         }
 
-
-        initialised = (currency != null && registrationFee != null);
+        initialised = (currency != null && applicationFee != null && allocationFee != null && registrationFee != null);
     }
 
     @Override
@@ -68,4 +85,11 @@ public final class DomainCreateFeeResponseExtension extends ResponseExtension {
         return registrationFee;
     }
 
+    public BigDecimal getAllocationFee() {
+        return allocationFee;
+    }
+
+    public BigDecimal getApplicationFee() {
+        return applicationFee;
+    }
 }
--- src/test/unit/java/com/ausregistry/jtoolkit2/se/fee/DomainApplicationFeeResponseExtensionTest.java
+++ src/test/unit/java/com/ausregistry/jtoolkit2/se/fee/DomainApplicationFeeResponseExtensionTest.java
@@ -1,16 +1,15 @@
 package com.ausregistry.jtoolkit2.se.fee;
 
+import org.junit.Test;
 import com.ausregistry.jtoolkit2.se.DomainCreateResponse;
 import com.ausregistry.jtoolkit2.se.ResponseExtension;
 import com.ausregistry.jtoolkit2.xml.XMLDocument;
 import com.ausregistry.jtoolkit2.xml.XMLParser;
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class DomainCreateFeeResponseExtensionTest {
+public class DomainApplicationFeeResponseExtensionTest {
 
     private static final XMLParser PARSER = new XMLParser();
 
@@ -18,8 +17,8 @@ public class DomainCreateFeeResponseExtensionTest {
     public void shouldGetFeeExtension() throws Exception {
         final String dnsForm = "xn--xha91b83h.com";
         final DomainCreateResponse response = new DomainCreateResponse();
-        final DomainCreateFeeResponseExtension feeResponseExtension =
-                new DomainCreateFeeResponseExtension(ResponseExtension.CREATE);
+        final DomainApplicationFeeResponseExtension feeResponseExtension =
+                new DomainApplicationFeeResponseExtension(ResponseExtension.CREATE);
         final XMLDocument doc =
                 PARSER.parse(getCreateResponseExpectedXml(dnsForm, true));
 
@@ -29,6 +28,8 @@ public class DomainCreateFeeResponseExtensionTest {
         assertTrue("Fee extension should have been initialised",
                 feeResponseExtension.isInitialised());
         assertEquals("USD", feeResponseExtension.getCurrency());
+        assertEquals("10.00", feeResponseExtension.getApplicationFee().toPlainString());
+        assertEquals("15.00", feeResponseExtension.getAllocationFee().toPlainString());
         assertEquals("30.00", feeResponseExtension.getRegistrationFee().toPlainString());
 
     }
@@ -50,35 +51,37 @@ public class DomainCreateFeeResponseExtensionTest {
         final StringBuilder result = new StringBuilder();
         result.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
         result.append("<epp xmlns=\"urn:ietf:params:xml:ns:epp-1.0\"");
-        result.append(    " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"");
-        result.append(    " xsi:schemaLocation=\"urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd\">");
-        result.append(    "<response>");
-        result.append(        "<result code=\"1000\">");
-        result.append(            "<msg>Command completed successfully</msg>");
-        result.append(        "</result>");
-        result.append(        "<resData>");
-        result.append(            "<domain:creData xmlns:domain=\"urn:ietf:params:xml:ns:domain-1.0\"");
-        result.append(                " xsi:schemaLocation=\"urn:ietf:params:xml:ns:domain-1.0 domain-1.0.xsd\">");
-        result.append(                "<domain:name>" + domainName + "</domain:name>");
-        result.append(                "<domain:crDate>1999-04-03T22:00:00.0Z</domain:crDate>");
-        result.append(                "<domain:exDate>2001-04-03T22:00:00.0Z</domain:exDate>");
-        result.append(            "</domain:creData>");
-        result.append(        "</resData>");
+        result.append(" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"");
+        result.append(" xsi:schemaLocation=\"urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd\">");
+        result.append("<response>");
+        result.append("<result code=\"1000\">");
+        result.append("<msg>Command completed successfully</msg>");
+        result.append("</result>");
+        result.append("<resData>");
+        result.append("<domain:creData xmlns:domain=\"urn:ietf:params:xml:ns:domain-1.0\"");
+        result.append(" xsi:schemaLocation=\"urn:ietf:params:xml:ns:domain-1.0 domain-1.0.xsd\">");
+        result.append("<domain:name>" + domainName + "</domain:name>");
+        result.append("<domain:crDate>1999-04-03T22:00:00.0Z</domain:crDate>");
+        result.append("<domain:exDate>2001-04-03T22:00:00.0Z</domain:exDate>");
+        result.append("</domain:creData>");
+        result.append("</resData>");
 
         if (feeExtension) {
             result.append("<extension>");
             result.append("<fee:creData xmlns:fee=\"urn:ietf:params:xml:ns:fee-0.6\">");
             result.append("<fee:currency>" + "USD" + "</fee:currency>");
-            result.append("<fee:fee>" + "30.00" + "</fee:fee>");
+            result.append("<fee:fee description=\"Application Fee\">" + "10.00" + "</fee:fee>");
+            result.append("<fee:fee description=\"Allocation Fee\">" + "15.00" + "</fee:fee>");
+            result.append("<fee:fee description=\"Registration Fee\">" + "30.00" + "</fee:fee>");
             result.append("</fee:creData>");
             result.append("</extension>");
         }
 
-        result.append(        "<trID>");
-        result.append(            "<clTRID>ABC-12345</clTRID>");
-        result.append(            "<svTRID>54321-XYZ</svTRID>");
-        result.append(        "</trID>");
-        result.append(    "</response>");
+        result.append("<trID>");
+        result.append("<clTRID>ABC-12345</clTRID>");
+        result.append("<svTRID>54321-XYZ</svTRID>");
+        result.append("</trID>");
+        result.append("</response>");
         result.append("</epp>");
         return result.toString();
     }
